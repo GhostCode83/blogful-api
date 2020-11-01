4d2c62fbaaae344e29e94663e098ebcbd149ee40
Randy Douglas <randy.e.douglas@gmail.com>
2020-11-01 16:36:26 -0500

    /api prefix and refactoring

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const ArticlesService = require('./articles-service')
+const articlesRouter = require('./articles/articles-router')
+const errorHandler = require('./error-handler')
 
 const app = express()
-const jsonParser = express.json()
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -17,57 +17,12 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/articles', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getAllArticles(knexInstance)
-    .then(articles => {
-      res.json(articles)
-    })
-    .catch(next)
-})
-
-app.get('/articles/:article_id', (req, res, next) => {
-  const knexInstance = req.app.get('db')
-  ArticlesService.getById(knexInstance, req.params.article_id)
-    .then(article => {
-      if (!article) {
-        return res.status(404).json({
-          error: { message: `Article doesn't exist` }
-        })
-      }
-      res.json(article)
-    })
-    .catch(next)
-})
+app.use('/api/articles', articlesRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
 
-app.post('/articles', jsonParser, (req, res, next) => {
-  const { title, content, style } = req.body
-  const newArticle = { title, content, style }
-  ArticlesService.insertArticle(
-    req.app.get('db'),
-    newArticle
-  )
-    .then(article => {
-      res
-        .status(201)
-        .location(`/articles/${article.id}`)
-        .json(article)
-    })
-    .catch(next)
-})
+app.use(errorHandler)
 
-app.use(function errorHandler(error, req, res, next) {
-  let response
-  if (NODE_ENV === 'production') {
-    response = { error: { message: 'server error' } }
-  } else {
-    console.log(error)
-    response = { message: error.message, error }
-  }
-  res.status(500).json(response)
-})
 module.exports = app
